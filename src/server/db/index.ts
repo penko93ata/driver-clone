2d64311766420518dd43aa9e561387f37cf9ffdc
Penko Atanasov <atanasovpenko@gmail.com>
2025-03-08 21:58:21 +0200

    Adding database

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,29 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
 import { env } from "~/env";
-import * as schema from "./schema";
+import { createPool, type Pool } from "mysql2/promise";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const conn =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(client, { schema });
+conn.addListener("error", (err) => {
+  console.error("Database connection error", err);
+});
+
+export default conn;
