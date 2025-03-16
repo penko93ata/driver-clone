6bad52ff776500dd3837c742dee65494d022546d
Penko Atanasov <atanasovpenko@gmail.com>
2025-03-16 12:04:50 +0200

    Placeholder homepage + updating readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { db } from "~/server/db";
-import {
-  files_table as filesSchema,
-  folders_table as foldersSchema,
-} from "~/server/db/schema";
-import DriveContents from "./drive-contents";
-
 export default async function GoogleDriveClone() {
-  const folders = await db.select().from(foldersSchema);
-  const files = await db.select().from(filesSchema);
-
-  return <DriveContents files={files} folders={folders} />;
+  return <>HomePage</>;
 }
